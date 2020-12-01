48f33a82ee75ee26e885777f30d5e39c9fda6bc1
Rohit Chaudhary <40576340+rorochaudhary@users.noreply.github.com>
2020-12-01 15:03:46 -0800

    some setup

---
+++ sudoku.py
@@ -0,0 +1,11 @@
+# Name: Rohit Chaudhary
+# Course: CS 325 - Analysis of Algorithms
+# HW 8: Portfolio Assignment
+# Date: 12/7/20
+# Description: For this project I chose to implement a 9x9 Sudoku solution verifier. The verifier takes as input a user-submitted solution.txt and determines whether solution.txt is a valid solution certifiate according to the rules of Sudoku. solution.py contains a 9x9 multi-dimensional array containing digit values 0-9 (where 0 denotes an empty position).
+
+def check_solution(certificate):
+    """iterates over certificate and determines whether certificate is a valid solution according to Sudoku rules: each row, each column, and each non-overlapping 3x3 grid must contain values between 1 and 9"""
+
+    pass
+
